6ddd0a9b1234aa7a4b6de8d7d7e6dc4d8cdc30ca
Nicol <nvojacek@gmail.com>
2019-05-27 18:40:10 +0200

    Changes to docker image and use env url

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,4 @@
+import os
 import requests
 from flask import Flask
 from flask_cors import CORS
@@ -15,7 +16,8 @@ TOKEN = API_CREDENTIALS['token']
 def create_webhook():
     payload = {
         'key': API_CREDENTIALS['key'],
-        'callbackURL': 'http://89c9d538.ngrok.io/webhooks/cards',
+        # 'callbackURL': 'http://89c9d538.ngrok.io/webhooks/cards',
+        'callbackURL': os.environ["NGROK_URL"],
         'idModel': '5ce2ae27e2de5319951360e0',
         'description': 'Webhook to watch board movement'
     }
