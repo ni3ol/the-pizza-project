e6a6115b27ac731869bf99b9548335588d38e556
Nicol Vojacek <nvojacek@gmail.com>
2019-05-28 10:39:13 +0200

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,11 +13,10 @@ import routes
 TOKEN = API_CREDENTIALS['token']
 
 
-def create_webhook():
+def create_webhook(NGROK_URL):
     payload = {
         'key': API_CREDENTIALS['key'],
-        # 'callbackURL': 'http://89c9d538.ngrok.io/webhooks/cards',
-        'callbackURL': os.environ["NGROK_URL"],
+        'callbackURL': NGROK_URL + '/webhooks/cards',
         'idModel': '5ce2ae27e2de5319951360e0',
         'description': 'Webhook to watch board movement'
     }
